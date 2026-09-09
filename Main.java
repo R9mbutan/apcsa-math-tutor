fd0d2f9e979a8853275c90b533a391c2830e53e2
Tran Phong <2157170@jeffcoschools.us>
2026-09-09 09:00:49 -0600

    added starter for QuadTool

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi your code works now your welcome");
+        System.out.println("hi your code works now you\'re welcome");
     }    
 }
---
+++ QuadTool.java
@@ -0,0 +1,9 @@
+public class QuadTool {
+    public int coefA = 1;
+    public int coefB = 1;
+    public int coefC = 1;
+
+    public void parseFunc(String fn){
+        
+    }
+}
