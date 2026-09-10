4ae30b5f8d7d25da8957ed0b75a802df7875b660
Tran Phong <2157170@jeffcoschools.us>
2026-09-10 09:05:40 -0600

    working on QuadTool parser

--- QuadTool.java
+++ QuadTool.java
@@ -4,6 +4,11 @@ public class QuadTool {
     public int coefC = 1;
 
     public void parseFunc(String fn){
-        
+        coefA = Integer.parseInt(fn.substring(0, fn.indexOf("x")));
+        String afterA = fn.substring(fn.indexOf("x^2", fn.length()-1));
+        //String afterA = fn;
+        System.out.println(afterA);
+        coefB = Integer.parseInt(afterA.substring(0, afterA.indexOf('x')));
+        System.out.println(coefA + ", " + coefB + ", " + coefC);
     }
 }
