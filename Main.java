b76dd3f1e2728e05ae8e23c766b5f412c8ab2644
Tran Phong <2157170@jeffcoschools.us>
2026-09-15 08:46:50 -0600

    Added parsetool and will test soon

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi your code works now you\'re welcome");
+        //System.out.println("hi your code works now you\'re welcome");
         QuadTool bud = new QuadTool();
         bud.parseFunc("3x^2+5x+4");
+        bud.parseFunc("40x^2+583x+344");
+        try {bud.parseFunc("81feyfc93u");}
+        catch(Exception e) { System.out.println("Please input in proper format!");}
     }    
 }
--- QuadTool.java
+++ QuadTool.java
@@ -4,11 +4,10 @@ public class QuadTool {
     public int coefC = 1;
 
     public void parseFunc(String fn){
-        coefA = Integer.parseInt(fn.substring(0, fn.indexOf("x")));
-        String afterA = fn.substring(fn.indexOf("x^2", fn.length()-1));
-        //String afterA = fn;
-        System.out.println(afterA);
-        coefB = Integer.parseInt(afterA.substring(0, afterA.indexOf('x')));
-        System.out.println(coefA + ", " + coefB + ", " + coefC);
+        coefA = Integer.parseInt(fn.substring(0, fn.indexOf("x^2")));
+        String afterA = fn.substring(fn.indexOf("x^2")+4, fn.length());
+        coefB = Integer.parseInt(afterA.substring(0, afterA.indexOf("x")));
+        coefC = Integer.parseInt(afterA.substring(afterA.indexOf("x")+2, afterA.length()));  
+        /*testing*/System.out.println(coefA + ", " + coefB + ", " + coefC);
     }
 }
