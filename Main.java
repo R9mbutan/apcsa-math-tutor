673af8b6fd39cdf4d4793b856512d73e667a5e7b
Rambutan <phong.935.tran@gmail.com>
2026-09-16 00:56:48 +0000

    accepts all sorts of wonky inputs now

--- Main.java
+++ Main.java
@@ -2,10 +2,16 @@ public class Main {
     public static void main(String[] args) {
         //System.out.println("hi your code works now you\'re welcome");
         QuadTool bud = new QuadTool();
-        bud.testing("3x^2+5x+4");
-        bud.testing("40x^2+583x+344");
-        bud.testing("o3ri3fgue");
-        bud.testing("x^2+2x+3");
+        bud.testing("x^2+2x+3"); //normal
+        bud.testing("-x^2-2x-3"); //negative
+        bud.testing("10x^2+20x+30"); //double digit
+        bud.testing("x^2+2x"); //no c Y
+        bud.testing("x^2+3"); //no b N
+        bud.testing("2x+3"); //no a Y
+        bud.testing("3"); //only c - return should 0, 0, 3 N
+        bud.testing("x^2"); //only a - should return 1, 0, 0 N
+        bud.testing("2x"); //only b - should return 0, 2, 0 Y
+        
 
     }    
 }
--- QuadTool.java
+++ QuadTool.java
@@ -1,14 +1,35 @@
 public class QuadTool {
-    public int coefA = 1;
-    public int coefB = 1;
-    public int coefC = 1;
+    public int coefA = 0;
+    public int coefB = 0;
+    public int coefC = 0;
 
     //updates the coefficients of A, B, and C based on the given argument (NO TRY/CATCH)
     public void parseFunc(String fn){
-        coefA = Integer.parseInt(fn.substring(0, fn.indexOf("x^2")));
-        String afterA = fn.substring(fn.indexOf("x^2")+4, fn.length());
-        coefB = Integer.parseInt(afterA.substring(0, afterA.indexOf("x")));
-        coefC = Integer.parseInt(afterA.substring(afterA.indexOf("x")+2, afterA.length()));  
+        //for coefA
+        //if coefA is there:
+        if (fn.indexOf("x^2") != -1) {
+            if (fn.substring(0, fn.indexOf("x^2")).equals("")) coefA = 1;
+            else if (fn.substring(0, fn.indexOf("x^2")).equals("-")) coefA = -1;
+            else coefA = Integer.parseInt(fn.substring(0, fn.indexOf("x^2")));
+            //String afterA = fn.substring(fn.indexOf("x^2")+3, fn.length());
+        }
+        else {
+            coefA = 0;
+            //String afterA = fn;
+        }
+        // sets afterA to the argument without x^2 term
+        String afterA = (fn.indexOf("x^2") != -1) ? fn.substring(fn.indexOf("x^2")+3, fn.length()) : fn;
+        //for coefB
+        //if coefB is there
+        if (afterA.indexOf("x") != -1) {
+            if (afterA.substring(0, afterA.indexOf("x")).equals("")) coefB = 1;
+            else if (afterA.indexOf("x") == -1) coefB = 0;
+            else coefB = Integer.parseInt(afterA.substring(0, afterA.indexOf("x")));
+        }
+        else coefB = 0;
+        //for coefC
+        if (afterA.substring(afterA.indexOf("x")+1, afterA.length()).equals("")) coefC = 0;
+        else coefC = Integer.parseInt(afterA.substring(afterA.indexOf("x")+1, afterA.length()));  
     }
 
     //returns whether an argument is acceptable
