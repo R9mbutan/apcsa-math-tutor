684ada4ab81f05ae0b415cef39e00aa7b4788fdf
Rambutan <phong.935.tran@gmail.com>
2026-09-16 01:10:07 +0000

    added QuadraticSolver, works for now

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //System.out.println("hi your code works now you\'re welcome");
-        QuadTool bud = new QuadTool();
+        /*QuadTool bud = new QuadTool();
         bud.testing("x^2+2x+3"); //normal
         bud.testing("-x^2-2x-3"); //negative
         bud.testing("10x^2+20x+30"); //double digit
@@ -10,7 +10,9 @@ public class Main {
         bud.testing("2x+3"); //no a Y
         bud.testing("3"); //only c - return should 0, 0, 3 N
         bud.testing("x^2"); //only a - should return 1, 0, 0 N
-        bud.testing("2x"); //only b - should return 0, 2, 0 Y
+        bud.testing("2x"); //only b - should return 0, 2, 0 Y */
+        QuadraticSolver bod = new QuadraticSolver();
+        System.out.println(bod.answer("x^2+5x+5"));
         
 
     }    
---
+++ QuadraticSolver.java
@@ -0,0 +1,16 @@
+public class QuadraticSolver extends QuadTool{
+    private double root1;
+    private double root2;
+
+    public void solve(double a, double b, double c) {
+        root1 = (-b + Math.sqrt(Math.pow(b,2) - 4 * a * c)) / (2*a) ;
+        root2 = (-b - Math.sqrt(Math.pow(b,2) - 4 * a * c)) / (2*a) ;
+    }
+
+    public String answer(String fn){
+        parseFunc(fn);
+        solve(coefA, coefB, coefC);
+        return "Roots are: " + root1 + " and " + root2;
+    }
+    
+}
